d726177b651dc456fcecb57db2e72542d229242f
Gabriele Maurina <gabriele@maurina.lan>
2020-09-30 21:56:53 +0200

    Working prototype.

--- resources/script.js
+++ resources/script.js
@@ -1,3 +1,5 @@
+const MAX = Number.MAX_SAFE_INTEGER
+
 const PLATFORMS = 'platforms'
 const ATHLETES = 'athletes'
 const RESULTS = 'results'
@@ -12,9 +14,92 @@ const EXPIRY = {expiry : 60 * 60 * 24 * 365}
 
 
 
+//   ALGORITHM
+
 const computeDistribution = (platforms, athletes) => {
-	
-	return [[14, 15],[60,70]]
+	athletes.sort((a, b) => a-b)
+
+	const N = athletes.length
+	const K = platforms
+	const min = Math.floor(N/K)
+	const max = min + 1
+	const mod = N % K
+
+	if(N <= K){
+		groups = []
+		for(k = 0; k < K; k++){
+			if(k < N) groups.push([athletes[k]])
+			else groups.push([])
+		}
+		return groups
+	}
+	else if(mod === 0){
+		groups = []
+		for(k = 0; k < K; k++){
+			groups.push([])
+			for(n = 0; n < min; n++){
+				groups[k].push(athletes[k * min + n])
+			}
+		}
+		return groups
+	}
+	else{
+		memo = []
+		for(var n=0; n<N; n++){
+			memo[n] = []
+			for(var k=0; k<K; k++){
+				memo[n][k] = [false, false, MAX]
+			}
+		}
+
+		f = (n, k) => {
+			if(n == N){
+				if(k == K) return 0
+				else return MAX
+			}
+			else if(k >= K){
+				return MAX
+			}
+			else if(memo[n][k][0]){
+				return memo[n][k][2]
+			}
+			else{
+				var best = MAX
+				if(n + min <= N){
+					const delta = Math.max(f(n + min, k + 1), athletes[n + min - 1] - athletes[n])
+					if(delta < best){
+						best = delta
+						memo[n][k][1] = false
+					}
+				}
+				if(mod && n + max <= N){
+					const delta = Math.max(f(n + max, k + 1), athletes[n + max - 1] - athletes[n])
+					if(delta < best){
+						best = delta
+						memo[n][k][1] = true
+					}
+				}
+				memo[n][k][0] = true
+				memo[n][k][2] = best
+				return best
+			}
+		}	
+		
+		const best = f(0,0)
+
+		groups = []
+		var n = 0
+		for(var k=0; n < N; k++){
+			const next = memo[n][k][1] ? n + max : n + min
+			groups[k] = []
+			for(i = n; i < next; i++){
+				groups[k].push(athletes[i])
+			}
+			n = next
+		}
+
+		return groups
+	}
 }
 
 const updateResults = () => {
@@ -28,7 +113,7 @@ const updateResults = () => {
 		div = document.createElement('div')
 		div.setAttribute('class', 'results')
 		h1 = document.createElement('h1')
-		h1.innerText = g.join(' ')
+		h1.innerText = g.length ? g.join(' ') : 'Vuoto'
 		div.appendChild(h1)
 		RESULTS_DIV.appendChild(div)
 	})
@@ -37,6 +122,8 @@ const updateResults = () => {
 
 
 
+
+//   PLATFORMS
 
 const parse_int = (value) => {
 	if(value){
@@ -52,7 +139,7 @@ if(value){
 	PLATFORMS_INPUT.value = value
 }
 
-PLATFORMS_INPUT.onchange = () => {
+PLATFORMS_INPUT.oninput = () => {
 	value = parse_int(PLATFORMS_INPUT.value)
 	if(value){
 		Cookies.set(PLATFORMS, value, EXPIRY)
@@ -63,6 +150,7 @@ PLATFORMS_INPUT.onchange = () => {
 
 
 
+//   ATHLETES LIST
 
 const parse_athletes = (value) => {
 	if(value){
@@ -95,5 +183,7 @@ ATHLETES_INPUT.oninput = () => {
 
 
 
+
+//   INITIAL UPDATE
 
 updateResults()
